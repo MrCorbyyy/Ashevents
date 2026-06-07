b541b3082b1035cd0ef68b0689c4cd9de144a7e2
Obeng Richard <crby12rich@gmail.com>
2026-06-07 00:46:16 +0000

    feat: create PackagesPage component with birthday packages, roadmap, and contact channels

--- src/pages/PackagesPage.tsx
+++ src/pages/PackagesPage.tsx
@@ -1,11 +1,13 @@
 import { BirthdayPackages } from "../components/BirthdayPackages";
-import { Contact } from "../components/Contact";
+import { ContactChannels } from "../components/ContactChannels";
+import { EventRoadmap } from "../components/EventRoadmap";
 
 export default function PackagesPage() {
   return (
     <div className="pt-20">
       <BirthdayPackages isPreview={false} />
-      <Contact />
+      <EventRoadmap />
+      <ContactChannels />
     </div>
   );
 }
